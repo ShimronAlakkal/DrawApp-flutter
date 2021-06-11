7d30eb44d464788061ce00af4f1a44e067dab405
Shimron <60049707+ShimronAlakkal@users.noreply.github.com>
2021-06-11 14:58:53 +0530

    Code refactoring and new feature additions

--- lib/tools/model.dart
+++ lib/tools/model.dart
@@ -10,16 +10,20 @@ class Painter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    penTool(this.points, canvas, size);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+  void penTool(List<List<DataPoints>> points, Canvas canvas, Size size) {
     for (int i = 0; i < this.points.length; i++) {
       for (int j = 0; j < this.points[i].length - 1; j++) {
         canvas.drawLine(points[i][j].points, points[i][j + 1].points,
             points[i][j].painterObject);
       }
     }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
   }
 }
